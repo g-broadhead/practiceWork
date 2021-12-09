327b8e7e307a1fff6e8f84a470e7ca56f3e18587
Garrett Broadhead <gwbroadhead@gmail.com>
2021-12-09 13:39:23 -0800

    changes

---
+++ Site/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/home', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'index.html'))
+  })
+
+app.get('/music', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'music.html'))
+  res.json(music)
+})
+const music = {
+  band: 'Dragonforce',
+  genre: 'Power Metal',
+  album: 'Inhuman Rampage'
+}
+  
+  
+
+app.get('/movie', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'movie.html'))
+  res.json(movies)
+})
+const movies = {
+  name: 'Shrek',
+  genre: 'Animated Masterpiece',
+  rating: '11/10'
+}
+  
+
+
+
+
+
+
+app.listen(3000)
+
---
+++ subtract.js
@@ -0,0 +1,12 @@
+const subtract = (a, b) => {
+  a = parseInt(a)
+  b = parseInt(b)
+
+  if (isNaN(a) || isNaN(b)) {
+    return null
+  }
+
+  return a - b
+}
+
+module.exports = subtract
